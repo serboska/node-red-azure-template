8482febc7506891f24ba2c944642b51108b22618
Aleksi Volanen <aleksi.volanen@siilim170602.local>
2017-09-01 14:07:54 +0300

    changed script version in deployment template, added missing sudo commands to shell script

--- install_node-red.sh
+++ install_node-red.sh
@@ -2,10 +2,11 @@
 # This script can be used together with azure deployment template to install Node-Red and its prerequirements
 # finally Node-Red is started
 
-sudo apt-get install nodejs-legacy
-sudo apt-get install npm
+sudo apt-get update -y
+sudo apt-get install -y nodejs-legacy
+sudo apt-get install -y npm
 sudo npm install -g --unsafe-perm node-red node-red-admin
-npm install -g node-red-contrib-azure-iot-hub
+sudo npm install -g node-red-contrib-azure-iot-hub
 
 # start Node-Red
 node-red
