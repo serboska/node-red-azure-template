d96b460f37166efbefab51efc689b27fcae7ec5b
Aleksi Volanen <aleksi.volanen@siilim170602.local>
2017-09-04 15:24:06 +0300

    added node-red-admin install, removed sudos

--- install_node-red.sh
+++ install_node-red.sh
@@ -2,11 +2,11 @@
 # This script can be used together with azure deployment template to install Node-Red and its prerequirements
 # finally Node-Red is started
 
-sudo DEBIAN_FRONTEND=noninteractive apt-get update -y
-sudo apt-get install -y nodejs
-sudo apt-get install -y npm
-sudo npm install -g node-red
-sudo npm install -g node-red-contrib-azure-iot-hub
+DEBIAN_FRONTEND=noninteractive apt-get update -y
+DEBIAN_FRONTEND=noninteractive apt-get install -y nodejs-legacy
+DEBIAN_FRONTEND=noninteractive apt-get install -y npm
+DEBIAN_FRONTEND=noninteractive npm install -g --unsafe-perm node-red node-red-admin
+DEBIAN_FRONTEND=noninteractive npm install -g --unsafe-prem node-red-contrib-azure-iot-hub
 
 # start Node-Red
 screen node-red
