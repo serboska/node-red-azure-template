215a0eab00440e8d52b0dc239c535a9ca2a6f10b
Aleksi Volanen <aleksi.volanen@siilim170602.local>
2017-09-04 13:35:22 +0300

    fixed variable references, added node-red to start in screen

--- install_node-red.sh
+++ install_node-red.sh
@@ -2,11 +2,11 @@
 # This script can be used together with azure deployment template to install Node-Red and its prerequirements
 # finally Node-Red is started
 
-sudo apt-get update -y
-sudo apt-get install -y nodejs-legacy
+sudo DEBIAN_FRONTEND=noninteractive apt-get update -y
+sudo apt-get install -y nodejs
 sudo apt-get install -y npm
-sudo npm install -g --unsafe-perm node-red node-red-admin
+sudo npm install -g node-red
 sudo npm install -g node-red-contrib-azure-iot-hub
 
 # start Node-Red
-node-red
+screen node-red
