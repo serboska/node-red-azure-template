99bf560871b1bb3bb9429ce157042c8664b05058
Aleksi Volanen <aleksi.volanen@siilim170602.local>
2017-09-04 16:22:03 +0300

    added init script

--- install_node-red.sh
+++ install_node-red.sh
@@ -2,11 +2,35 @@
 # This script can be used together with azure deployment template to install Node-Red and its prerequirements
 # finally Node-Red is started
 
-DEBIAN_FRONTEND=noninteractive apt-get update -y
-DEBIAN_FRONTEND=noninteractive apt-get install -y nodejs-legacy
-DEBIAN_FRONTEND=noninteractive apt-get install -y npm
-DEBIAN_FRONTEND=noninteractive npm install -g --unsafe-perm node-red node-red-admin
-DEBIAN_FRONTEND=noninteractive npm install -g --unsafe-prem node-red-contrib-azure-iot-hub
+apt-get update -y
+curl -sL https://deb.nodesource.com/setup_4.x | -E bash -
+apt-get install -y nodejs build-essential
+npm install -g --unsafe-perm node-red
+npm install -g --unsafe-prem node-red-contrib-azure-iot-hub
 
-# start Node-Red
-screen node-red
+# register Node-Red as a Service and start it
+cat <<EOT >> /etc/systemd/system/node-red.service
+[Unit]
+Description=Node-RED
+After=syslog.target network.target
+
+[Service]
+ExecStart=/usr/bin/node-red
+Restart=on-failure
+KillSignal=SIGINT
+
+# log output to syslog as 'node-red'
+SyslogIdentifier=node-red
+StandardOutput=syslog
+
+# non-root user to run as
+WorkingDirectory=/home/$1/
+User=$1
+Group=$1
+
+[Install]
+WantedBy=multi-user.target
+EOT
+
+systemctl enable node-red
+systemctl start node-red
